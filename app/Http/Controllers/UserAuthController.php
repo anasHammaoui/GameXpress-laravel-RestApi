a240aa4d80ad7ad85d0ae1e05a8c1d53cb2cc226
anashm <anashammaoui07@gmail.com>
2025-03-11 07:33:48 +0000

    Complete sanctum authentification

---
+++ app/Http/Controllers/UserAuthController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class UserAuthController extends Controller
+{
+    // :register function 
+    public function register(Request $request){
+        // validate data
+        $data = Validator::make($request -> all(),[
+            'name' => 'required|string',
+            'email' => 'required|email|string|unique:users',
+            'password' => 'required|min:8'
+        ]);
+        // :check if data is correct 
+        if ($data -> fails()){
+            return response() -> json([
+                "errors" => $data -> errors()
+            ],422);
+          }  else {
+            // if it's correct register the user
+            User::create([
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'password' => Hash::make($data['password']),
+            ]);
+            return response() -> json([
+                "message" => 'Account created successfully'
+            ],200);
+          }
+       
+    }
+    // login funciton
+    public function login(Request $request){
+        $data = Validator::make($request -> all(),[
+            'email' => 'required|email',
+            'password' => 'required|min:8'
+        ]);
+    
+        if ($data -> fails()){
+            return response() -> json([
+                "errors" => $data -> errors()
+            ],422);
+        }
+        $user = User::where('email',$request -> email) -> first();
+        
+        if (!$user || !Hash::check($request -> password, $user -> password)){
+            return response() -> json([
+                "message" => "Email or password is incorrect"
+            ],401);
+        }
+      
+        $token = $user -> createToken($request -> email) -> plainTextToken;
+        return response() -> json([
+            "message" => "You loged in successfully",
+            "access_token" => $token
+        ]);
+    }
+    // logout funciton
+    public function logout(){
+        if (auth('sanctum') -> check()){
+            auth() -> user()-> tokens() -> delete();
+            return response() -> json([
+                "message" => "Logged out successfully"
+            ]);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\UserAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::post('/v1/admin/login',[UserAuthController::class,"login"]);
+Route::post('/v1/admin/register',[UserAuthController::class,"register"]);
+Route::post('/v1/admin/logout',[UserAuthController::class,"logout"]) -> middleware('auth:sanctum');
