9bf314c6c1f9b3061280ca4dcd0b290dafd56c58
anashm <anashammaoui07@gmail.com>
2025-03-14 12:02:32 +0000

    GA-9 complete adding caategory with unit test

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 class CategoryController extends Controller
 {
     /**
@@ -12,31 +13,36 @@ class CategoryController extends Controller
      */
     public function index()
     {
-    //    if (auth('sanctum') -> user() -> can('view_categories')){
+       if (auth('sanctum') -> user() -> can('view_categories')){
         $categories = Category::all();
         return response() -> json([
             'categries' => $categories
         ],200);
-    //    }
-    //    return response() -> json([
-    //    'message' => 'You dont have acces to this page'
-    // ],403);
+       }
+       return response() -> json([
+       'message' => 'You dont have acces to this page'
+    ],403);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        // if (auth('sanctum') -> user() -> can('create_categories')){
+            $validate = Validator::make($request -> all(),[
+                'name' => 'required|min:4' 
+            ]);
+            if ($validate -> fails()){
+                return response() -> json(['message' => $validate -> errors()],422);
+            }
+           $category = Category::create([
+                'name'=> $request -> name,
+                'slug' => Str::slug($request -> name)
+            ]);
+            return response() -> json(['message'=> 'category created successfully', 'category' => $category],200);
+        // }
     }
 
     /**
