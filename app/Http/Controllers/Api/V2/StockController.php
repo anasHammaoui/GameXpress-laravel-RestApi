cd5e3f6d20c546b77e84edc324368fefbeda10f5
samira kibous <kbs130999@gmail.com>
2025-03-17 13:33:46 +0000

    comparaison avec le stocke

---
+++ app/Http/Controllers/Api/V2/StockController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api\V2;
+
+use App\Models\Product;
+
+use Illuminate\Http\Request;
+
+class StockController
+{
+    public function compareToStock($product_id, Request $request)
+    {
+        $product = Product::find($product_id);
+        if ($product) {
+
+            $quantity = $request->quantity;
+            if ($quantity === null || $quantity <= 0) {
+                return response()->json(['message' => 'ajouter une valeur valide', 'status' => 'error'], 400);
+            }
+            $cart = Cart::where('product_id', $product_id)->first();
+            if ($cart) {
+                if ($quantity > $product->stock) {
+                    return response()->json(['message' => 'stock insuffisant', 'status' => 'error'], 400);
+                } else {
+                    return response()->json(['message' => 'stock suffisant', 'status' => 'success'], 200);
+                }
+            } else {
+                return response()->json(['message' => 'produit non trouvé dans le panier', 'status' => 'error'], 404);
+            }
+        }
+    }
+}
