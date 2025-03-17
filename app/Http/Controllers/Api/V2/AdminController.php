fe1c2592b30f19b0f6de3d76b16e545dfabfc060
LatrachDev <m.latrach.youcode@gmail.com>
2025-03-17 13:58:47 +0000

    GV-5 create the controller of the admin

---
+++ app/Http/Controllers/Api/V2/AdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api\V2;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    // Permissions management
+
+
+
+    // Roles management
+
+    
+}
