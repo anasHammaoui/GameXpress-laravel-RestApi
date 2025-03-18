e5e4bc114de6dd9af75475a4c9716c714dc10f7f
LatrachDev <m.latrach.youcode@gmail.com>
2025-03-18 11:25:42 +0000

    GV-7 handle the Cart items expire

---
+++ app/Console/Commands/ClearExpiredCart.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Cart;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class ClearExpiredCart extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:clear-expired-cart';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Remove items from cart that are older than 48 hours';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $expirationTime = Carbon::now()->subHours(48);
+        
+        $deleted = Cart::where('created_at', '<', $expirationTime)->delete();
+        $this->info("$deleted items removed from cart.");
+    }
+}
+
